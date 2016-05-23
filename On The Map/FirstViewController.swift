d3e929c0688e21212772cae8ee662ba9e565e561
mnienaber <mnienaber@mnienaber-macbookpro3.local>
2016-05-23 17:59:54 +1000

    Initial Commit

---
+++ On The Map/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  On The Map
+//
+//  Created by Michael Nienaber on 23/05/2016.
+//  Copyright © 2016 Michael Nienaber. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
